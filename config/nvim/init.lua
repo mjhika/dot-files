fd691f0dc6a83b4e04f1c566839b3ae45ab377a5
mjhika <1zzt6ovh@mojica.anonaddy.com>
2026-02-16 22:42:26 -0500

    feat(nvim)!: add servers
    add bashls, expert, zls
    add beautysh, clj-kondo, jq, stylua
    remove elixirls, jdtls

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -565,11 +565,12 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
+				bashls = {},
 				clangd = {},
 				clojure_lsp = {},
-				elixirls = {},
+				expert = {},
 				gopls = {},
-				jdtls = {},
+				zls = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 
 				lua_ls = {
@@ -603,7 +604,10 @@ require("lazy").setup({
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
+				"beautysh",
+				"clj-kondo",
+				"jq",
+				"stylua",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
